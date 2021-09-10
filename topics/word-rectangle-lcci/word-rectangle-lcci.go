0bd70f583a6209baabf188c4a7c416d5303a3c28
xuejunc <deerhunter0837@gmail.com>
2021-09-10 16:05:25 +0800

    新增题解

---
+++ topics/word-rectangle-lcci/word-rectangle-lcci.go
@@ -0,0 +1,91 @@
+// 面试题 17.25. 单词矩阵
+//给定一份单词的清单，设计一个算法，创建由字母组成的面积最大的矩形，其中每一行组成一个单词(自左向右)，每一列也组成一个单词(自上而下)。不要求这些单词在清单里连续出现，但要求所有行等长，所有列等高。
+//
+//如果有多个面积最大的矩形，输出任意一个均可。一个单词可以重复使用。
+//
+//示例 1:
+//
+//输入: ["this", "real", "hard", "trh", "hea", "iar", "sld"]
+//输出:
+//[
+//   "this",
+//   "real",
+//   "hard"
+//]
+//示例 2:
+//
+//输入: ["aa"]
+//输出: ["aa","aa"]
+//说明：
+//
+//words.length <= 1000
+//words[i].length <= 100
+//数据保证单词足够随机
+//通过次数1,382提交次数2,803
+//
+// @author xuejunc deerhunter0837@gmail.com
+// @create 9/9/21 2:46 PM
+package word_rectangle_lcci
+
+import (
+	"math"
+	"strings"
+)
+
+// 暴力算法
+func maxRectangle(words []string) []string {
+	dict := make(map[string]bool)
+	wordsByLength := make(map[int][]string)
+	maxLen := math.MinInt32
+	for _, word := range words {
+		dict[word] = true
+		l := len(word)
+		wordsByLength[l] = append(wordsByLength[len(word)], word)
+		if l > maxLen {
+			maxLen = l
+		}
+	}
+	for l := maxLen; l > 0; l-- {
+		if _, ok := wordsByLength[l]; !ok {
+			continue
+		}
+		var rectangles [][]string
+		getRectangles(dict, wordsByLength[l], l, 0, []string{}, &rectangles)
+		if len(rectangles) > 0 {
+			return rectangles[len(rectangles)-1]
+		}
+	}
+	return nil
+}
+
+func isValid(dict map[string]bool, rectangle []string) bool {
+	n := len(rectangle[0])
+	cols := make([]strings.Builder, n)
+	for _, word := range rectangle {
+		for i := 0; i < n; i++ {
+			cols[i].WriteByte(word[i])
+		}
+	}
+	for _, col := range cols {
+		if !dict[col.String()] {
+			return false
+		}
+	}
+	return true
+}
+
+func getRectangles(dict map[string]bool, words []string, length, i int, rectangle []string, rectangles *[][]string) {
+	if i > 0 && isValid(dict, rectangle) {
+		arr := make([]string, len(rectangle))
+		copy(arr, rectangle)
+		*rectangles = append(*rectangles, arr)
+	}
+	if i == length {
+		return
+	}
+	for _, word := range words {
+		rectangle = append(rectangle, word)
+		getRectangles(dict, words, length, i+1, rectangle, rectangles)
+		rectangle = rectangle[:len(rectangle)-1]
+	}
+}
---
+++ topics/word-rectangle-lcci_02/word-rectangle-lcci_02.go
@@ -0,0 +1,112 @@
+// 面试题 17.25. 单词矩阵
+//给定一份单词的清单，设计一个算法，创建由字母组成的面积最大的矩形，其中每一行组成一个单词(自左向右)，每一列也组成一个单词(自上而下)。不要求这些单词在清单里连续出现，但要求所有行等长，所有列等高。
+//
+//如果有多个面积最大的矩形，输出任意一个均可。一个单词可以重复使用。
+//
+//示例 1:
+//
+//输入: ["this", "real", "hard", "trh", "hea", "iar", "sld"]
+//输出:
+//[
+//   "this",
+//   "real",
+//   "hard"
+//]
+//示例 2:
+//
+//输入: ["aa"]
+//输出: ["aa","aa"]
+//说明：
+//
+//words.length <= 1000
+//words[i].length <= 100
+//数据保证单词足够随机
+//通过次数1,382提交次数2,803
+//
+// @author xuejunc deerhunter0837@gmail.com
+// @create 9/9/21 2:46 PM
+package word_rectangle_lcci_02
+
+type TrieNode struct {
+	IsLeaf   bool
+	Children [26]*TrieNode
+}
+
+func (t *TrieNode) Insert(s string) {
+	cur := t
+	for _, r := range s {
+		i := r - 'a'
+		if cur.Children[i] == nil {
+			cur.Children[i] = &TrieNode{}
+		}
+		cur = cur.Children[i]
+	}
+	cur.IsLeaf = true
+}
+
+// 利用前缀树来剪枝
+func maxRectangle(words []string) []string {
+	trie := &TrieNode{}
+	wordsByLength := make(map[int][]string)
+	maxLen := 0
+	for _, word := range words {
+		trie.Insert(word)
+		l := len(word)
+		wordsByLength[l] = append(wordsByLength[len(word)], word)
+		if l > maxLen {
+			maxLen = l
+		}
+	}
+
+	tries := make([]*TrieNode, maxLen)
+	for i := 0; i < len(tries); i++ {
+		tries[i] = trie
+	}
+	var ans []string
+	for l := maxLen; l > 0; l-- {
+		if l*l <= area(ans) {
+			break
+		}
+		if _, ok := wordsByLength[l]; !ok {
+			continue
+		}
+		getRectangles(wordsByLength[l], 0, []string{}, tries, &ans)
+	}
+	return ans
+}
+
+func getRectangles(words []string, i int, rectangle []string, tries []*TrieNode, ans *[]string) {
+	length := len(words[0])
+	for _, word := range words {
+		var nextTries []*TrieNode
+		valid, allLeaf := true, true
+		for i := 0; i < length; i++ {
+			nextTrie := tries[i].Children[word[i]-'a']
+			if nextTrie == nil {
+				valid = false
+				break
+			}
+			nextTries = append(nextTries, nextTrie)
+			if !nextTrie.IsLeaf {
+				allLeaf = false
+			}
+		}
+		if valid {
+			rectangle = append(rectangle, word)
+			if allLeaf && area(rectangle) > area(*ans) {
+				*ans = make([]string, len(rectangle))
+				copy(*ans, rectangle)
+			}
+			if i+1 < length {
+				getRectangles(words, i+1, rectangle, nextTries, ans)
+			}
+			rectangle = rectangle[:len(rectangle)-1]
+		}
+	}
+}
+func area(rectangle []string) int {
+	if rectangle == nil {
+		return 0
+	}
+	return len(rectangle) * len(rectangle[0])
+}
